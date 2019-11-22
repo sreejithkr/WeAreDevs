3b3374bee2ccda499df292a9efd4e57c0384ab7b
saravanakumar <ssarawanakumar@gmail.com>
2019-11-23 01:56:11 +0530

    Update HomePresenter.swift

--- WeAreDevs/Views/Home/HomePresenter.swift
+++ WeAreDevs/Views/Home/HomePresenter.swift
@@ -6,12 +6,15 @@
 //  Copyright © 2019 SKR. All rights reserved.
 //
 
-import Foundation
+protocol HomePresenterProtocol {
+    func isValid(_ email: String) -> Bool
+}
 
-class HomePresenter {
+class HomePresenter: HomePresenterProtocol {
     func isValid(_ email: String) -> Bool {
         let pattern = "[A-Z0-9a-z._]+@[A-Z0-9a-z]{2,16}?\\.[A-Za-z]{2,5}"
         let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+
         return predicate.evaluate(with: email)
     }
 }
